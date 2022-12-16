a63699358e5658578174ff109a43e46edf0baeb2
Elia <elia.jabbour.bn@gmail.com>
2022-12-16 20:15:43 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,47 @@
-import '../styles/globals.css'
+import '../styles/globals.scss'
+import { ThemeProvider } from 'next-themes'
+import { createBrowserSupabaseClient } from '@supabase/auth-helpers-nextjs' //importing the supabase client
+import { SessionContextProvider } from '@supabase/auth-helpers-react' //Importing the session context provider from the 
+import { useState } from 'react'
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+import "@fullcalendar/common/main.css";
+import "@fullcalendar/daygrid/main.css";
+import "@fullcalendar/timegrid/main.css";
+
+// Fonts Inter
+import { Inter } from '@next/font/google'
+const inter = Inter({
+  subsets: ['latin'],
+})
+
+
+function MyApp({ Component, pageProps }) {
+  const [supabase] = useState(() => createBrowserSupabaseClient())
+  const getLayout = Component.getLayout || ((page) => page)
+
+  return getLayout(
+
+    <SessionContextProvider supabaseClient={supabase} initialSession={pageProps.initialSession} >
+      <ThemeProvider forcedTheme={Component.theme || null} defaultTheme="light">
+
+        <main className={inter.className}>
+          <Component {...pageProps} />
+        </main>
+
+      </ThemeProvider>
+    </SessionContextProvider>
+  )
 }
+
+export default MyApp
+
+
+// Test.getLayout = function getLayout(page) {
+//   return (
+//       <TestLayout>
+//           <TestLayout2>
+//               {page}
+//           </TestLayout2>
+//       </TestLayout>
+//   )
+// }
