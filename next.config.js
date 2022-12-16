fac0adceb1244ea8f8f466b24cfccd312e263a69
Elia <elia.jabbour.bn@gmail.com>
2022-12-16 20:13:11 +0100

    next

--- next.config.js
+++ next.config.js
@@ -1,6 +1,68 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
 
-module.exports = nextConfig
+//used for the scss modules
+const path = require('path');
+
+
+const withTM = require("next-transpile-modules")([
+  "@fullcalendar/common",
+  "@babel/preset-react",
+  "@fullcalendar/common",
+  "@fullcalendar/daygrid",
+  "@fullcalendar/interaction",
+  "@fullcalendar/react",
+  "@fullcalendar/timegrid"
+]);
+
+
+module.exports = withTM({
+  // custom config goes here
+  reactStrictMode: true,
+
+  //svg support
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack"],
+    });
+    return config;
+  },
+
+  //scss support
+  sassOptions: {
+    includePaths: [path.join(__dirname, "styles")],
+  },
+
+  //image support
+  images: {
+    domains: ["ixmpfpsjhdyjnhmhjglg.supabase.co"],
+  },
+
+
+});
+
+
+// const nextConfig = {
+//   reactStrictMode: true,
+
+//   //svg support
+//   webpack(config) {
+//     config.module.rules.push({
+//       test: /\.svg$/,
+//       use: ["@svgr/webpack"],
+//     });
+//     return config;
+//   },
+
+//   //scss support
+//   sassOptions: {
+//     includePaths: [path.join(__dirname, "styles")],
+//   },
+
+//   //image support
+//   images: {
+//     domains: ["ixmpfpsjhdyjnhmhjglg.supabase.co"],
+//   },
+// }
+
+// module.exports = withTM(nextConfig);
