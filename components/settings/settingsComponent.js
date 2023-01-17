30480705acd8e64b165dfa35f6bb72b775e53646
Elia <elia.jabbour.bn@gmail.com>
2023-01-17 11:32:58 +0100

    Update settingsComponent.js

--- components/settings/settingsComponent.js
+++ components/settings/settingsComponent.js
@@ -5,16 +5,32 @@ import styles from "/styles/settingsComponent.module.scss"
 import ButtonWithIcon from '../buttons'
 import AccountSetting from '/components/settings/Account'
 import GeneralSetting from '/components/settings/General'
+import CalendarSetting from '/components/settings/CalendarSetting'
 
 //icons
 import CloseIcon from "/public/close.svg"
 import Account from "/public/Profile.svg"
 import General from "/public/Settings.svg"
+import Calendar from "/public/Calendar.svg"
 
 //global state
-import { useSettingsContext } from "/layouts/stateStore"
+import { useStateStoreContext } from "/layouts/stateStore"
+
+//supabase
+import { useUser, useSupabaseClient, useSession } from '@supabase/auth-helpers-react'
+
+//theme
+import { useTheme } from 'next-themes'
+
+//toast
+import toast, { Toaster } from 'react-hot-toast';
+
 
 export default function Setting() {
+
+    const supabase = useSupabaseClient()
+    const session = useSession()
+    const user = useUser()
 
     const [account, setAccount] = useState(true);
     const [general, setGeneral] = useState(false);
@@ -23,7 +39,155 @@ export default function Setting() {
     const [notesSettings, setNotesSettings] = useState(false);
     const [ai, setAi] = useState(false);
 
-    const [showSettings, setShowSettings] = useSettingsContext();
+    const [showSettings, setShowSettings, shortcutsPanel, setShortcutsPanel, settings, setSettings, saveButton, setSaveButton, settingsCopy, setSettingsCopy] = useStateStoreContext();
+    const { theme, setTheme } = useTheme()
+
+
+    const [loading, setLoading] = useState(false)
+
+    //Get data from database
+    async function GetProfileData() {
+        try {
+            setLoading(true)
+            const { data, error } = await supabase
+                .from('profiles')
+                .select(`*`)
+                .eq('id', user.id)
+
+            if (data) {
+                const dataFromDatabase = {
+                    //variable name : name in data
+                    FullName: data[0].full_name,
+                    UserName: data[0].username,
+                    Website: data[0].website,
+                    Theme: data[0].mode,
+                    syncTheme: data[0].syncTheme,
+                    FirstDayOfTheWeek: data[0].firstDayOfWeek,
+                    TimeZone: data[0].timeZone,
+                    BeginTimeDay: data[0].BeginTimeDay,
+                    EndTimeDay: data[0].EndTimeDay,
+                    ShowWeekends: data[0].ShowWeekends,
+                    avatar_url: data[0].avatar_url
+                }
+
+                setSettings(
+                    {
+                        ...settings,
+                        ...dataFromDatabase
+                    }
+                )
+                setSettingsCopy(
+                    {
+                        ...settings,
+                        ...settingsCopy,
+                        ...dataFromDatabase
+                    }
+                )
+
+                //set theme
+                if (data[0].syncTheme && data[0].mode != theme) {
+                    setTheme(data[0].mode)
+                }
+
+
+
+            }
+        } catch (err) {
+            console.log(err)
+        }
+        finally {
+            setLoading(false)
+        }
+    }
+
+    function handleURL() {
+        if (settings.avatar_url.includes("https") == false) {
+            downloadTheAvatar(settings.avatar_url)
+        }
+    }
+
+    async function downloadTheAvatar(avatarUrl) {
+
+        try {
+            const { data, error } = await supabase.storage
+                .from('avatars')
+                .download(avatarUrl)
+
+            // create a url for the file object to display in the browser
+            const url2 = URL.createObjectURL(data)
+            setSettings(
+                {
+                    ...settings,
+                    profileImageUrl: url2
+                }
+            )
+            setSettingsCopy(
+                {
+                    ...settings,
+                    ...settingsCopy,
+                    profileImageUrl: url2
+                }
+            )
+
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    // update data in database
+    async function updateData() {
+
+        const { data, error } = await supabase
+            .from('profiles')
+            .update({
+                //Name in data  : variable name 
+                full_name: settings.FullName,
+                username: settings.UserName,
+                website: settings.Website,
+                mode: settings.Theme,
+                syncTheme: settings.syncTheme,
+                firstDayOfWeek: settings.FirstDayOfTheWeek,
+                timeZone: settings.TimeZone,
+                BeginTimeDay: settings.BeginTimeDay,
+                EndTimeDay: settings.EndTimeDay,
+                ShowWeekends: settings.ShowWeekends,
+                avatar_url: settings.filepath
+            })
+            .eq('id', user.id)
+
+        if (error) {
+            console.log(error)
+        }
+
+    }
+
+    // console.log("settings " + JSON.stringify(settings))
+    // console.log("settingsCopy " + JSON.stringify(settingsCopy))
+    //compare two lists of useStatuses and return true if they are different
+    function compareSettings() {
+        if (JSON.stringify(settings) !== JSON.stringify(settingsCopy)) {
+            setSaveButton(true)
+        } else {
+            setSaveButton(false)
+        }
+
+    }
+
+    useEffect(() => {
+        handleURL()
+    }, [settings.avatar_url])
+
+
+    useEffect(() => {
+        compareSettings()
+    }, [settings])
+
+
+    useEffect(() => {
+        GetProfileData()
+    }, [])
+
+
     const [data, setData] = useState();
 
     //handle account, show account settings
@@ -31,8 +195,8 @@ export default function Setting() {
 
         setAccount(true);
         setGeneral(false);
-        setTasksSettings(false);
         setCalendarSettings(false);
+        setTasksSettings(false);
         setNotesSettings(false);
         setAi(false);
 
@@ -43,8 +207,20 @@ export default function Setting() {
 
         setAccount(false);
         setGeneral(true);
-        setTasksSettings(false);
         setCalendarSettings(false);
+        setTasksSettings(false);
+        setNotesSettings(false);
+        setAi(false);
+
+    }
+
+    //handle calendar, show calendar settings
+    const handleCalendar = () => {
+
+        setAccount(false);
+        setGeneral(false);
+        setCalendarSettings(true);
+        setTasksSettings(false);
         setNotesSettings(false);
         setAi(false);
 
@@ -59,8 +235,11 @@ export default function Setting() {
         if (general) {
             setData(<GeneralSetting />)
         }
+        if (calendarSettings) {
+            setData(<CalendarSetting />)
+        }
 
-    }, [account])
+    }, [account, general, calendarSettings])
 
 
     return (
@@ -80,6 +259,7 @@ export default function Setting() {
 
                             <ButtonWithIcon icon={<Account />} text="Account" onClick={handleAccount} active={account} />
                             <ButtonWithIcon icon={<General />} text="General" onClick={handleGeneral} active={general} />
+                            <ButtonWithIcon icon={<Calendar />} text="Calendar" onClick={handleCalendar} active={calendarSettings} />
 
                         </div>
 
@@ -87,11 +267,45 @@ export default function Setting() {
 
                 </div>
 
+
+
                 <div className={styles.rightPanel} >
                     {data}
+
+                    {saveButton ?
+
+                        <div className={styles.savaAndCancelButtons}>
+                            <button className={`${styles.cancelButton} ${styles.buttong}`}
+                                onClick={
+                                    () => {
+                                        setSettings(settingsCopy)
+                                        setSaveButton(false)
+                                    }
+                                }
+
+                            >Cancel</button>
+                            <button className={`${styles.saveButton} ${styles.buttong}`}
+                                onClick={
+                                    () => {
+                                        updateData()
+                                        setSettingsCopy(settings)
+                                        setSaveButton(false)
+                                    }
+                                }
+                            >Save
+                            </button>
+
+
+                        </div>
+
+
+
+                        : null}
                 </div>
 
+                <Toaster position="bottom-right" reverseOrder={false} />
             </div>
+
         </div>
     )
 }
