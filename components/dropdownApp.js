29fc827f72061c74a4ff948ab91b83013c9690f1
Elia <elia.jabbour.bn@gmail.com>
2023-01-17 11:33:05 +0100

    Update dropdownApp.js

--- components/dropdownApp.js
+++ components/dropdownApp.js
@@ -1,13 +1,19 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import styles from "/styles/dropdownApp.module.scss"
 import { useState, useEffect } from 'react'
 import { useUser, useSupabaseClient, useSession } from '@supabase/auth-helpers-react'
 import { useRouter } from 'next/router'
 import ButtonWithIcon from './buttons'
 import Image from 'next/image'
+
+import { useStateStoreContext } from "/layouts/stateStore"
+
 //icons
 import Settings from "/public/Settings.svg"
 import Logout from "/public/Logout.svg"
+import Home from "/public/home.svg"
+import Message from "/public/message.svg"
+import Shortcut from "/public/shortcut.svg"
 
 //theme
 import { useTheme } from 'next-themes'
@@ -25,6 +31,8 @@ export default function DropdownApp({ settingsState }) {
     const [collapse, setCollapse] = useState(true);
     const dropdownIteam = collapse ? `${styles.dropdownListClose} ${styles.dropdownList}` : `${styles.dropdownListOpen} ${styles.dropdownList}`;
 
+    const [showSettings, setShowSettings, shortcutsPanel, setShortcutsPanel, settings, setSettings, saveButton, setSaveButton, settingsCopy, setSettingsCopy] = useStateStoreContext();
+
     //sign out function
     async function handleSignOut() {
 
@@ -38,41 +46,8 @@ export default function DropdownApp({ settingsState }) {
         }
     }
 
-    const [avatarUrl, setAvatarUrl] = useState(process.env.NEXT_PUBLIC_IMAGE_URL)
 
-    //we will replace this with global variables
-    // async function getProfileImage() {
-    //     console.log("getProfileImage")
-    //     try {
-    //         let { data, error, } = await supabase
-    //             .from('profiles')
-    //             .select(`avatar_url`)
 
-    //         if (data) {
-    //             setUrl(data.avatar_url)
-    //         }
-    //         if (error) {
-    //             console.log(error)
-    //         }
-
-    //         const { data2, error2 } = await supabase.storage
-    //             .from('avatars')
-    //             .download(
-    //                 url2
-    //             )
-    //         if (data2) {
-    //             setAvatarUrl(URL.createObjectURL(data2))
-    //         }
-    //         if (error2) {
-    //             console.log(error)
-    //         }
-
-    //     }
-    //     catch (error) {
-    //         console.log("error: " + error)
-
-    //     }
-    // }
 
 
     //theme change, click counter until 3
@@ -113,28 +88,81 @@ export default function DropdownApp({ settingsState }) {
     }
 
 
-    useEffect(() => {
-        // getProfileImage()
-    }, [])
 
     return (
-        <div className={styles.dropdown}>
+        <div className={styles.dropdown} id="dropdownHolder">
 
-            <div className={styles.profileImage} onClick={() => setCollapse(!collapse)} >
+            <div className={styles.profileImage} onClick={
+                () => {
+                    setCollapse(!collapse)
+                    // collapse ? addDropdownList() : removeDropdownList()
+                }
+            }
+            >
                 <Image
-                    src={avatarUrl}
+                    src={settings.avatar_url.includes("http") ? settings.avatar_url : settings.profileImageUrl}
                     alt="Profile Image"
                     fill
                     className={styles.image}
                 />
             </div>
 
-            <div className={dropdownIteam}>
-                <ButtonWithIcon icon={<Settings />} text="Settings" onClick={() => { settingsState(), setCollapse(!collapse) }} />
-                <ButtonWithIcon icon={<Settings />} text={`${theme} Mode`} onClick={counter} />
-                <ButtonWithIcon icon={<Logout />} text="Sign Out" onClick={() => { handleSignOut(), setCollapse(!collapse) }} />
+            <div className={dropdownIteam} id="dropdownList"
+            //  ref={dropdownList}
+            >
+                <ButtonWithIcon
+                    icon={<Settings />}
+                    text="Settings"
+                    onClick={() => {
+                        settingsState(),
+                            setCollapse(!collapse)
+                    }}
+                />
+
+                <ButtonWithIcon
+                    icon={<Settings />}
+                    text={`${theme} Mode`}
+                    onClick={counter}
+                />
+
+                <ButtonWithIcon
+                    icon={<Shortcut />}
+                    text="Shortcut"
+                    onClick={() => {
+                        // router.push("/shortcut"), setCollapse(!collapse)
+                        setShortcutsPanel(!shortcutsPanel)
+                        setCollapse(!collapse)
+                    }}
+                />
+
+                <ButtonWithIcon
+                    icon={<Home />}
+                    text="Home Page"
+                    onClick={() => {
+                        router.push("/"), setCollapse(!collapse)
+                    }}
+                />
+
+                <ButtonWithIcon
+                    icon={<Message />}
+                    text="Contact Us"
+                    onClick={() => {
+                        router.push("/about"), setCollapse(!collapse)
+                    }}
+                />
+
+                <ButtonWithIcon
+                    icon={<Logout />}
+                    text="Sign Out"
+                    onClick={() => {
+                        handleSignOut(),
+                            setCollapse(!collapse)
+                    }}
+                />
+
+
                 {/* version number */}
-                <div className={styles.version}>v0.0.9</div>
+                <div className={styles.version}>v0.1.9</div>
 
             </div>
 
