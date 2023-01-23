1f3486be9bfa12405c9faa748237e36f29945e36
Elia <elia.jabbour.bn@gmail.com>
2023-01-23 23:36:16 +0100

    update loadingLine.js

--- components/loadingLine.js
+++ components/loadingLine.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import styles from "/styles/loadingLine.module.scss"
-import Navbar from "components/navbar"
+import Navbar from "/components/navbar"
 
 //loading animation 
-export default function loadingLine({notYourNote}) {
+export default function loadingLine({ notYourNote }) {
     return (
         <div>
-            <Navbar back={notYourNote}/>
+            <Navbar back={notYourNote} />
             <center><strong>{notYourNote ? "Can't find this note on your profile !" : "You're not yet signed in !"}</strong></center>
             <div className={styles.holder}>
                 <div className={styles.line}></div>
