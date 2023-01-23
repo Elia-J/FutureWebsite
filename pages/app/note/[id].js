1d168613368f1a9f056f4c102bb554923e4e9888
hugokrul <hugo.krul@gmail.com>
2023-01-23 22:07:52 +0100

    delete "}"

--- pages/app/note/[id].js
+++ pages/app/note/[id].js
@@ -407,7 +407,6 @@ export default function Notes({ notes }) {
         var b = s.split(/\D+/);
         return new Date(Date.UTC(b[0], --b[1], b[2], b[3], b[4], b[5], b[6]));
     }
-    }
 
     useEffect(() => {
         //search only afther the user is done with typing
