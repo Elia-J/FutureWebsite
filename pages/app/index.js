fdd650eb7ed0dc0ee110e9ef862818bcafd9b82b
Elia <elia.jabbour.bn@gmail.com>
2023-01-26 14:07:02 +0100

    Update index.js

--- pages/app/index.js
+++ pages/app/index.js
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from 'react'
 import { useRouter } from 'next/router'
-import Link from 'next/link'
 import styles from "/styles/app.module.scss"
 
 import { useUser, useSupabaseClient, useSession } from '@supabase/auth-helpers-react'
@@ -11,8 +10,6 @@ import { EventProvider } from "/layouts/stateStoreEvents"
 import ListOfTasks from "/components/listOfTasks"
 import ListOfNotes from "/components/listOfNotes"
 import Calendar from '/components/calendar'
-
-import Trash from "/public/trash.svg"
 
 import { useStateStoreContext } from "/layouts/stateStore"
 
@@ -31,13 +28,14 @@ export default function IndexApp() {
     const [todoPanelToggle, setTodoPanelToggle] = useState(false);
     const todoPanelStyle = todoPanelToggle ? `${styles.todoPnael} ${styles.todoPnaelOpen}` : `${styles.todoPnael} ${styles.todoPnaelClose}`;
 
-    const [panelData, setPanelData] = useState()
-
-
+    const [panelData, setPanelData] = useState(
+        <div className={styles.todoList}>
+            <h2>Tasks</h2>
+            <ListOfTasks></ListOfTasks>
+        </div>
+    )
 
     const [showSettings, setShowSettings, shortcutsPanel, setShortcutsPanel, settings, setSettings, saveButton, setSaveButton, settingsCopy, setSettingsCopy, warningPanel, setWarningPanel] = useStateStoreContext();
-
-
 
     //Not done yet, redirect the user to sign in page if not logged in
     useEffect(() => {
@@ -46,95 +44,31 @@ export default function IndexApp() {
         }
     })
 
-
-
-    // useEffect(() => {
-    //     getAllTask()
-    //     getAllNotes()
-    //     switchPanel({ target: { value: "tasks" } })
-    // }, [todoPanelToggle])
-
-    // useEffect(() => {
-    //     getAllNotes()
-    // }, [removeNote])
-
-
-
-    // get all task from supabase
-    // async function getAllTask() {
-    //     const { data, error } = await supabase
-    //         .from('todos')
-    //         .select('*')
-    //         .order('title', { ascending: true })
-    //     // .eq('user_id', user.id)
-
-    //     if (data) {
-    //         setTasksData(data)
-    //     }
-
-    //     if (error) {
-    //         console.log(error)
-    //     }
-
-    //     else {
-    //         console.log(data)
-    //     }
-    // }
-
-    // //get all notes from supabase
-    // async function getAllNotes() {
-    //     const { data, error } = await supabase
-    //         .from('notesv2')
-    //         .select('*')
-    //         .order('title', { ascending: true })
-    //     // .eq('user_id', user.id)
-    //     if (data) {
-    //         setNotesData(data)
-    //         console.log(data)
-    //     }
-    //     if (error) {
-    //         console.log(error)
-    //     }
-    // }
-
-    // // Remove notes with id
-    // async function removeNote(id) {
-    //     const { data, error } = await supabase
-    //         .from('notesv2')
-    //         .delete()
-    //         .eq('id', id)
-    //     if (data) {
-    //         //array of todos
-    //         setData(data)
-    //     } if (error) {
-    //         console.log('error', error)
-    //         return
-    //     }
-    // }
-
     //switch between tasks and notes
     function switchPanel(event) {
         const panel = event.target.value
 
-        if (panel === "tasks") {
-            setPanelData(
-                <div className={styles.todoList}>
-                    <h2>Tasks</h2>
-                    <ListOfTasks></ListOfTasks>
-                </div>
-            )
-        }
-        if (panel === "notes") {
-            setPanelData(
-                <div className={styles.todoList}>
-                    <h2>Notes</h2>
-                    <ListOfNotes inApp={true}></ListOfNotes>
-                </div>
-            )
+        switch (panel) {
+            case "tasks":
+                setPanelData(
+                    <div className={styles.todoList}>
+                        <h2>Tasks</h2>
+                        <ListOfTasks></ListOfTasks>
+                    </div>
+                )
+                break;
+            case "notes":
+                setPanelData(
+                    <div className={styles.todoList}>
+                        <h2>Notes</h2>
+                        <ListOfNotes inApp={true}></ListOfNotes>
+                    </div>
+                )
+                break;
+            default:
+                break;
         }
     }
-
-
 
     if (session) {
         return (
@@ -158,9 +92,10 @@ export default function IndexApp() {
                         <div className={todoPanelStyle}>
 
 
-                            <div className={styles.todoListHeader}>
+                            <div >
                                 <button className={styles.buttonAdd} onClick={switchPanel} value="tasks">Task</button>
                                 <button className={styles.buttonAdd} onClick={switchPanel} value="notes">Notes</button>
+
 
                                 <div className={styles.todoListHeader}>
                                     {panelData}
