b77a1934c2f72d32c02b05fb34a453922b4758b6
Elia <elia.jabbour.bn@gmail.com>
2023-01-27 22:12:04 +0100

    Update deleteUser.js

--- pages/api/deleteUser.js
+++ pages/api/deleteUser.js
@@ -1,13 +1,12 @@
 import { supabaseAdmin } from "/lib/supasbaseClient.js"
 
-export default async function DeleteUser(req, res) {
+export default async function DeleteUserAccount(req, res) {
 
 
     const supabase = supabaseAdmin()
 
     //get the asscess token from the request
     const { access_token } = req.body
-
 
     //get the user id from the access_token and supabase
     const { data: { user }, error: error1 } = await supabase.auth.getUser(
@@ -21,6 +20,8 @@ export default async function DeleteUser(req, res) {
     const user_id = user.id
 
 
+
+    //############################################################ Delete the user data from the database - start #################
     //Delete the user from the todos table
     const { data: data5, error: error5 } = await supabase
         .from('todos')
@@ -31,7 +32,6 @@ export default async function DeleteUser(req, res) {
             message: "Error deleting user from todos table " + error5
         })
     }
-
 
     //Delete the user from the notesv2 table
     const { data: data4, error: error4 } = await supabase
@@ -44,22 +44,43 @@ export default async function DeleteUser(req, res) {
         })
     }
 
+    //delete the user from the folders table
+    const { data: data6, error: error6 } = await supabase
+        .from('folders')
+        .delete()
+        .eq('user_id', user_id)
+    if (error6) {
+        res.status(500).send({
+            message: "Error deleting user from folders table " + error6
+        })
+    }
+
+    //delete the user from the todosFolders table
+    const { data: data7, error: error7 } = await supabase
+        .from('todosFolders')
+        .delete()
+        .eq('user_id', user_id)
+    if (error7) {
+        res.status(500).send({
+            message: "Error deleting user from todosFolders table " + error7
+        })
+    }
 
     //Delete the user from the profiles table
     const { data: data3, error: error3 } = await supabase
         .from('profiles')
         .delete()
         .eq('id', user_id)
-
-
     if (error3) {
         res.status(500).send({
             message: "Error deleting user from profiles table " + error3
         })
     }
 
+    //############################################################ Delete the user data from the database - end #################
 
-    //Delete the user
+
+    //Delete the user from the supabase auth table
     const { data: data2, error: error2 } = await supabase.auth.admin.deleteUser(
         user_id
     )
