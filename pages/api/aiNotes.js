025698638c19e8fb6d6b49afab1e6cb8bc93a4fb
Elia <elia.jabbour.bn@gmail.com>
2023-01-27 23:01:23 +0100

    Update aiNotes.js

--- pages/api/aiNotes.js
+++ pages/api/aiNotes.js
@@ -1,20 +1,24 @@
 import React from "react";
 import { Configuration, OpenAIApi } from "openai";
 
+//configuration for the openai api
 const config = new Configuration({
     apiKey: process.env.OPENAI_API_KEY,
 });
 
+//create a new instance of the openai api
 const openai = new OpenAIApi(config);
 
 export default async function handler(req, res) {
 
+    //check if the secret key is correct
     if (req.body.key !== process.env.NEXT_PUBLIC_API_SECRET_KEY) {
         return res.status(401).json({ message: "Unauthorized" });
     }
 
     const { prompt } = req.body;
 
+    //create a completion
     const response = await openai.createCompletion({
         model: "text-davinci-003",
         prompt: `${prompt} ?`,
@@ -25,5 +29,6 @@ export default async function handler(req, res) {
         presence_penalty: 0,
     });
 
+    //send the response
     res.status(200).json(response.data);
 }
