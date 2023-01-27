187a2d6de1f682061a6572a45142536fc055a016
Elia <elia.jabbour.bn@gmail.com>
2023-01-27 23:01:20 +0100

    Update aiTasks.js

--- pages/api/aiTasks.js
+++ pages/api/aiTasks.js
@@ -1,20 +1,26 @@
 import React from "react";
 import { Configuration, OpenAIApi } from "openai";
 
+//configerqtion for the openai api
 const config = new Configuration({
     apiKey: process.env.OPENAI_API_KEY,
 });
 
+//create a new instance of the openai api
 const openai = new OpenAIApi(config);
 
+//this is the api route that will be called from the client side
 export default async function handler(req, res) {
 
+    //check if the secret key is correct
     if (req.body.key !== process.env.NEXT_PUBLIC_API_SECRET_KEY) {
         return res.status(401).json({ message: "Unauthorized" });
     }
 
+    //get the prompt from the request
     const { prompt } = req.body;
 
+    //create a completion
     const response = await openai.createCompletion({
         model: "text-davinci-003",
         prompt: `Create a to-do list that does not include numbering and -: ${prompt} \n`,//wihout numbering and - 
@@ -25,5 +31,6 @@ export default async function handler(req, res) {
         presence_penalty: 0,
     });
 
+    //send the response back to the client
     res.status(200).json(response.data);
 }
